9dcd807fa2aa71b2cd8d1da9d118567dcc87fa04
dinesh2003 <adhikaridinesh76@gmail.com>
2023-01-16 23:31:11 -0500

    Finished

---
+++ src/ProductReader.java
@@ -0,0 +1,92 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class ProductReader {
+    public static void main(String[] args) {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+        ArrayList<String> lines = new ArrayList<>();
+
+        final int FIELDS_LENGTH = 4;
+
+        String id, name, description;
+        double cost = 0;
+
+        try
+        {
+
+            File workingDirectory = new File(System.getProperty("user.dir"));
+
+            chooser.setCurrentDirectory(workingDirectory);
+
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+
+                InputStream in = new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+
+                int line = 0;
+                while(reader.ready())
+                {
+                    rec = reader.readLine();
+                    lines.add(rec);
+                    line++;
+                    System.out.println(rec);
+
+                }
+                reader.close();
+                System.out.println();
+
+
+                System.out.print(String.format("%-10s%-12s%-28s%s", "ID#", "Name", "Description","Cost"));
+                System.out.print("\n============================================================");
+                String[] fields;
+                for(String l:lines)
+                {
+                    fields = l.split(",");
+                    if(fields.length == FIELDS_LENGTH)
+                    {
+
+                        id = fields[0].trim();
+                        name = fields[1].trim();
+                        description = fields[2].trim();
+                        cost = Double.parseDouble(fields[3].trim());
+                        System.out.print(String.format("\n%-10s%-12s%-28s$%.2f", id, name, description, cost));
+                    }
+                    else
+                    {
+                        System.out.println("Found a record that may be corrupt: ");
+                        System.out.println();
+                    }
+                }
+                System.out.println();
+            }
+            else
+            {
+                System.out.println("Failed to choose a file to process");
+                System.out.println("Run the program again!");
+                System.exit(0);
+            }
+        }  // end of TRY
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
+
